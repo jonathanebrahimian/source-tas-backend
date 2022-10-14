3ee7b49bb754b94483cd4ce4daea4216ed5bb813
Temi Dosu <tdosu@smu.edu>
2022-10-14 14:45:32 -0500

    Add cors to backend

--- app/app.py
+++ app/app.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI, Depends
 
 from app.routes.classes import router as ClassRouter
 from fastapi.security import OAuth2PasswordBearer
-
+from fastapi.middleware.cors import CORSMiddleware
 from logzero import logger
 
 app = FastAPI()
-
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=['*']
+)
 app.include_router(ClassRouter, tags=["Class"], prefix="/class")
 
 @app.get("/", tags=["Root"])
