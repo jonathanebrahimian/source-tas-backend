9745a1ee22da38bd71f40644a230cea2d6c96e74
PierceClemmons <pclemmons@smu.edu>
2022-11-18 14:13:18 -0600

    Update database.py

--- app/database.py
+++ app/database.py
@@ -24,6 +24,15 @@ def class_helper(class_obj) -> dict:
         "labs": class_obj["labs"],
     }
 
+def application_helper(class_obj) -> dict:
+    return {
+        "email": class_obj["email"],
+        "name": class_obj["name"],
+        "time": class_obj["time"],
+        "resume": class_obj["resume"],
+        "classes": class_obj["classes"],
+    }
+
 
 # Retrieve all classes present in the database
 async def retrieve_classes(active: bool):
@@ -73,4 +82,45 @@ async def delete_class(id: str):
     class_obj = await class_collection.find_one({"_id": ObjectId(id)})
     if class_obj:
         await class_collection.delete_one({"_id": ObjectId(id)})
+        return True
+
+
+# Retrieve all applications present in the database
+async def retrieve_applications():
+    applications = []
+    async for application_obj in applications_collection.find():
+        applications.append(application_helper(application_obj))
+    return json.loads(json_util.dumps(applications))
+
+# Add a new application into to the database
+async def add_application(application_data: dict) -> dict:
+    application_obj = await applications_collection.insert_one(application_data)
+    new_application = await applications_collection.find_one({"_id": application_obj.inserted_id})
+    return application_helper(new_application)
+
+# Retrieve a application with a matching ID
+async def retrieve_application(id: str) -> dict:
+    application_obj = await applications_collection.find_one({"_id": ObjectId(id)})
+    if application_obj:
+        return application_helper(application_obj)
+
+# Update a application with a matching ID
+async def update_application(id: str, data: dict):
+    # Return false if an empty request body is sent.
+    if len(data) < 1:
+        return False
+    application_obj = await applications_collection.find_one({"_id": ObjectId(id)})
+    if application_obj:
+        updated_application = await applications_collection.update_one(
+            {"_id": ObjectId(id)}, {"$set": data}
+        )
+        if updated_application:
+            return True
+        return False
+
+# Delete a application from the database
+async def delete_application(id: str):
+    application_obj = await applications_collection.find_one({"_id": ObjectId(id)})
+    if application_obj:
+        await applications_collection.delete_one({"_id": ObjectId(id)})
         return True
